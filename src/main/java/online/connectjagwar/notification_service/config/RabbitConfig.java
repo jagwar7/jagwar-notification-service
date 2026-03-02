6045757fbd4e500c4cd834501bc697d2736fe1f6
BeeingDeveloper <com.official.akhter007@gmail.com>
2026-03-02 21:35:01 +0530

    SUCCESSFULLY SETUP RABBIT EC2 AND MADE CONNECTION

--- src/main/java/online/connectjagwar/notification_service/config/RabbitConfig.java
+++ src/main/java/online/connectjagwar/notification_service/config/RabbitConfig.java
@@ -1,7 +1,7 @@
 package online.connectjagwar.notification_service.config;
 
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -9,37 +9,65 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
 @Configuration
 public class RabbitConfig {
 
-    @Value("${rabbitmq.queue.name}")
-    private String queueName;
+    @Value ("${spring.rabbitmq.host}")
+    private String host;
+
+    @Value("${spring.rabbitmq.port}")
+    private int port;
+
+    @Value("${spring.rabbitmq.username}")
+    private String username;
+
+    @Value("${spring.rabbitmq.password}")
+    private String password;
+
+    @Value("${spring.rabbitmq.virtual-host}")
+    private String vhost;
+
 
     @Bean
-    public Queue notificationQueue() {
-        return new Queue(queueName, true);
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory factory = new CachingConnectionFactory();
+        
+        
+        factory.setHost(host);               // RABBIT HOST IP
+        factory.setPort(port);               // RABBIT PORT
+        factory.setUsername(username);       // RABBIT USER NAME
+        factory.setPassword(password);       // RABBIT PASSWORD
+        factory.setVirtualHost(vhost);       // Using the root vhost we cleaned up
+        
+        factory.setConnectionTimeout(15000);
+        return factory;
     }
 
+    @Bean
+    public TopicExchange notificationExchange() {
+        return new TopicExchange("notification_exchange");
+    }
+
+    @Bean
+    public Queue emailQueue() {
+        return new Queue("email_queue", true);
+    }
+
+    @Bean
+    public Binding emailBinding(Queue emailQueue, TopicExchange notificationExchange) {
+        return BindingBuilder.bind(emailQueue).to(notificationExchange).with("notification.email");
+    }
 
     @Bean
     public Jackson2JsonMessageConverter jsonMessageConverter() {
         return new Jackson2JsonMessageConverter();
     }
 
-
     @Bean
     public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
         RabbitTemplate template = new RabbitTemplate(connectionFactory);
         template.setMessageConverter(jsonMessageConverter());
         return template;
-    }
-
-
-    @Bean
-    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
-        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
-        factory.setConnectionFactory(connectionFactory);
-        factory.setMessageConverter(jsonMessageConverter());
-        return factory;
     }
 }
